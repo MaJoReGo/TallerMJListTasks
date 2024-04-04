587d633db9e44b2e2b1961fa9b0eeaeb178d613d
MaJoReGo <126532332+MaJoReGo@users.noreply.github.com>
2024-04-03 21:57:25 -0500

    Update Main.jsx

--- src/components/Layouts/Main/Main.jsx
+++ src/components/Layouts/Main/Main.jsx
@@ -1,30 +1,41 @@
-import React, { useState } from 'react';
-import { Form } from '../../Form/Form';
-import { Filter } from '../../Filter/Filter';
-import './Main.css';
+import React, { useState } from 'react'; 
+import { Form } from '../../Form/Form'; 
+import { Filter } from '../../Filter/Filter'; 
+import './Main.css'; 
 
 export const Main = () => {
-  const [tasks, setTasks] = useState([]);
-  const [filter, setFilter] = useState('all');
 
-  const handleCreateTask = (title, description) => {
-    const newTask = { title, description, completed: false };
+const [tasks, setTasks] = useState([]); 
+const [filter, setFilter] = useState('all'); 
+const handleCreateTask = (title, description) => {
+
+const newTask = { title, description, completed: false };
+
+    //esta sintaxis se utiliza para combinar múltiples arrays
+
     setTasks([...tasks, newTask]);
+    
+    //operador de propagación
   };
 
   const handleCheckboxChange = (index) => {
     const updatedTasks = [...tasks];
+    //se crea una copia
     updatedTasks[index].completed = !updatedTasks[index].completed;
     setTasks(updatedTasks);
   };
 
+ 
   const filteredTasks = tasks.filter(task => {
     if (filter === 'all') {
-      return true;
+      return true; 
     } else if (filter === 'completed') {
-      return task.completed;
+      return task.completed; 
     } else if (filter === 'pending') {
-      return !task.completed;
+      return !task.completed; 
+
+    //! invierte el valor booleano
+
     }
   });
 
@@ -37,13 +48,15 @@ export const Main = () => {
         <Filter onFilterChange={setFilter} />
       </div>
       <div className="task-container">
+        {/* Mapea y renderiza las tareas filtradas */}
         {filteredTasks.map((task, index) => (
           <div key={index} className={`task ${task.completed ? 'completed' : 'pending'}`}>
             <div className={`circle ${task.completed ? 'checked' : ''}`}></div>
+            {/*indica que se debe insertar valores de la expresión entre las llaves*/}
             <h3 className={task.completed ? 'completed' : ''}>{task.title}</h3>
             <p className={task.completed ? 'completed' : ''}>{task.description}</p>
             <label className="checkbox-container">
-              <input type="checkbox" checked={task.completed} onChange={() => handleCheckboxChange(index)} />
+              <input type="checkbox" checked={task.completed} onChange={() => handleCheckboxChange(index)} /> {/*controlador de evento*/}
               <span className="checkbox-custom"></span>
             </label>
           </div>
@@ -52,5 +65,3 @@ export const Main = () => {
     </div>
   );
 };
-
-export default Main;
