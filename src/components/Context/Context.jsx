1918593fb60632231211e62f881b629a9bd8ecb1
MaJoReGo <126532332+MaJoReGo@users.noreply.github.com>
2024-04-03 21:53:30 -0500

    ignorar este componente, no es usado en el codigo.

--- src/components/Context/Context.jsx
+++ src/components/Context/Context.jsx
@@ -1,12 +1,1 @@
-import { createContext, useState } from "react";
-
-export const tasksContext = createContext();
-
-export const TasksProvider = ({ children }) => {
-  const [tasks, setTasks] = useState(["Iniciando App"]);
-  return (
-    <tasksContext.Provider value={{ tasks, setTasks }}>
-      {children}
-    </tasksContext.Provider>
-  );
-};
+//ignotar este componente. Gracias :)
